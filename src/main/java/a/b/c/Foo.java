2dd8ec7e7cef40f1aaa86f46435a8c2bd4023164
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-06-29 12:21:19 +0200

    add

---
+++ src/main/java/a/b/c/Foo.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *
+ * <p>Created by irina on 6/29/2022.</p>
+ * <p>Project: maven-java-level-single</p>
+ * *
+ */
+public class Foo {
+}
---
+++ src/main/java/a/b/c/FooRecord.java
@@ -0,0 +1,5 @@
+package a.b.c;
+
+
+public record FooRecord(String a, String b) {
+}
